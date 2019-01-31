d5589b30f71795307f92d6bc6036a567e0cb102d
nerfymdog <47138818+nerfymdog@users.noreply.github.com>
2019-01-31 09:41:23 +0100

    Update function.js

--- function.js
+++ function.js
@@ -56,13 +56,11 @@ function refreshDeviceList(){
 
 function onDiscoverDevice(device){
 	//Make a list in html and show devises
-		if (device.name != "undefined")
-		{
-			var listItem = document.createElement('li'),
-			html = device.name+ "," + device.id;
-			listItem.innerHTML = html;
-			document.getElementById("bleDeviceList").appendChild(listItem);
-		}
+		var listItem = document.createElement('li'),
+		html = device.name+ "," + device.id;
+		listItem.innerHTML = html;
+		document.getElementById("bleDeviceList").appendChild(listItem);
+		
 }
 
 
