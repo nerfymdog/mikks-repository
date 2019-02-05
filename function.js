e55e5dbafc639caaed0147e02ed978f2062ee9c1
EILERTZAN <47137635+EILERTZAN@users.noreply.github.com>
2019-02-05 13:09:43 +0100

    ugulg

--- function.js
+++ function.js
@@ -47,7 +47,7 @@ function refreshDeviceList(){
 	//deviceList =[];
 	document.getElementById("bleDeviceList").innerHTML = ''; // empties the list
 	if (cordova.platformId === 'android') { // Android filtering is broken
-		ble.scan([], 5, onDiscoverDevice, onError);
+		ble.scan([], 5, onDiscoverDevice, onError, removeRadar);
 	} else {
 		//alert("Disconnected");
 		ble.scan([blue.serviceUUID], 5, onDiscoverDevice, onError);
